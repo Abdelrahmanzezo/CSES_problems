96cb767f11c463fd444ab9f333b6166af6b79729
AbdElrahman_Aziz <89615122+Abdelrahmanzezo@users.noreply.github.com>
2022-10-06 04:27:15 +0200

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -1,9 +1,8 @@
-def find_missing_num(n):
-    numbers = set(n)
-    result = []
-    for i in range(1,n[-1]):
-        if i not in numbers:
-            result.append(i)
-    return result
-list_of_missing_nums = [1, 2, 10]
-print(find_missing_num(list_of_missing_nums))
+n = int(input())
+list =[int(i) for i in input().split()]
+for i in range(1,n+1):
+    if i in list:
+        None
+    else:
+        print(i)
+        break
